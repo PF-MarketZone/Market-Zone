f88d448cb03868f05d078a3b0a56fdddfe36a4c2
Alfredo Lillo <alphonselillo@gmail.com>
2023-08-10 21:11:16 -0300

    Actualizando repo

--- api/src/models/Users.js
+++ api/src/models/Users.js
@@ -1,38 +1,40 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Videogame', {
-    id:{
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false
-    },
-    
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastName: {
+  sequelize.define(
+    "Videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      name: {
         type: DataTypes.STRING,
         allowNull: false,
-    },
-    password: {
+      },
+      lastName: {
         type: DataTypes.STRING,
         allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      birthdate: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
     },
-    email:{
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    birthdate: {
-      type: DataTypes.DATEONLY,
-      allowNull: false
-    }
-
-
-  },{ timestamps: false });
+    { timestamps: false }
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const userRouter = require("")
 
 const router = Router();
+
+router.get("/user", userRouter)
 
 module.exports = router;
