761bd1c810c3a8856456ac71284affb5c012756e
Alfredo Lillo <alphonselillo@gmail.com>
2023-08-11 16:14:40 -0300

    Creando modelos Tienda, Productos, Review

---
+++ api/src/models/Review.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    'Review',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nameUser: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }, // Deberia venir desde Users
+      rating: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: fasle,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    { timestamps: false }
+  );
+};
