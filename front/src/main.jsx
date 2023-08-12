aeec72357573958591e39f04def11db8b81c1505
Martín0211 <martin021191@gmail.com>
2023-08-12 13:40:39 -0600

    se envuelve app con StyleSheetManager para generar estilos dinámicamente en el DOM

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { StyleSheetManager } from 'styled-components';
 
 
 const container = document.getElementById('root');
@@ -9,8 +10,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
+     <StyleSheetManager shouldForwardProp={(prop) => !['index', 'rating'].includes(prop)}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </StyleSheetManager>
   </React.StrictMode>,
 );
