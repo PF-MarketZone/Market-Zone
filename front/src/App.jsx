98bfe83ce2cdf9daa38e25a13ecf455999a6377d
Martín0211 <martin021191@gmail.com>
2023-08-12 13:23:48 -0600

    se actualiza app y se incluyen fuente provicional, se importan y se usan Routes y Route, se genera ruta para detalle y se importa la view Detail

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,12 +1,32 @@
 import MainButton from './components/Buttons/MainButton'
-import './App.css'
+import Detail from './View/Detail/Detail'
+import { Routes, Route } from 'react-router-dom';
+//import './App.css'
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+      display: block;
+      margin: 0;
+      padding: 0;
+  }
+`;
+
 
 function App() {
-    return (
+
+  return (
     <>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin = "true"/>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"></link>
       <div>
-        <h1>Hola mundo</h1>
-        <MainButton text="Boton" route="/"/>
+        <Routes>
+          <Route path="/detail/:detailId" element={<Detail />} />
+        </Routes>
+        <div>
+          <MainButton text="Ir a Detalle" route="/detail/1" />
+        </div>
       </div>
     </>
   )
