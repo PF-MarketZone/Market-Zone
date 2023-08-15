5e00c3a7221e75dd6316956ecf7a79623ad30e86
Martín0211 <martin021191@gmail.com>
2023-08-15 16:32:12 -0600

    se agrego link para que redirija la card a detalles

--- front/src/components/CardProducto/CardProducto.jsx
+++ front/src/components/CardProducto/CardProducto.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import styles from "./CardProducto.module.css";
+import { Link } from 'react-router-dom';
 
-const CardProducto = ({ name, stock, images, price }) => {
+
+const CardProducto = ({ id, name, stock, images, price }) => {
+  console.log("Product ID:", id)
   return (
     <div className={styles.card}>
+      <Link className={styles.LinkCar} to={{
+        pathname: `/detail/${id}`,
+        state: { productDetails: { id, name, stock, images, price } }
+      }}>
       <div className={styles.imageContainer}>
         <Carousel>
           {images.map((image, index) => (
@@ -18,6 +25,7 @@ const CardProducto = ({ name, stock, images, price }) => {
       <h2>{name}</h2>
       <p>Stock: {stock}</p>
       <p>Precio: ${price}</p>
+    </Link>
     </div>
   );
 };
