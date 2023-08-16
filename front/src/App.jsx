722dd5265b202988567c97b471365af75f6cdf3e
Martín0211 <martin021191@gmail.com>
2023-08-15 22:41:58 -0600

    se elimino la nav de landing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,8 +7,10 @@ import TiendaDetalle from "./components/TiendaDetalle/TiendaDetalle";
 import Detail from './View/Detail/Detail'
 import Landing from "./View/Landing/Landing";
 import Add from "./View/addProduct/AddProduct";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   const [categoriaFiltrada, setCategoriaFiltrada] = useState();
 
   const handleSearch = (categoria) => {
@@ -17,25 +19,28 @@ function App() {
 
   return (
     <>
-    <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin = "true"/>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"></link>
-    <div className="App">
-      
-      <Nav onSearch={handleSearch} />
-      <Routes>
-        <Route path="/" element={<Landing />} />
+      <div className="App">
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"></link>
+
+        <div>
+          {location.pathname === '/' ? null : <Nav onSearch={handleSearch} />}
+        </div>
         
- 
-        <Route path="/home" element={<Home categoriaFiltrada={categoriaFiltrada} />} />
-        <Route path="/add" element={<Add />} />
-        <Route path="/:name" element={<TiendaDetalle />} />
-        <Route path="/detail/:detailId" element={<Detail />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+
+
+          <Route path="/home" element={<Home categoriaFiltrada={categoriaFiltrada} />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/:name" element={<TiendaDetalle />} />
+          <Route path="/detail/:detailId" element={<Detail />} />
+        </Routes>
+      </div>
     </>
   );
-  
+
 }
 export default App;
 
