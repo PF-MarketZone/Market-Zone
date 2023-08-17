993a80559d596e9985ea3e4e2ebd61dcbe362a89
diegofmoreno4 <dfmm17ykty@gmail.com>
2023-08-16 19:51:41 -0500

    carrito agregado

--- front/src/components/Nav/Nav.jsx
+++ front/src/components/Nav/Nav.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Nav.module.css";
 import SearchBar from "../searchBar/searchBar";
+import { useSelector } from "react-redux";
 
 const Nav = ({ onSearch }) => {
+  const cartItems = useSelector(state => state.cart);
+
   return (
     <div className={styles.navContainer}>
       <div className={styles.navbar}>
@@ -13,11 +16,12 @@ const Nav = ({ onSearch }) => {
         <Link to="/add">
           <button className={styles.button1}>Crear Producto</button>
         </Link>
+        <Link to="/cart">
+          <button className={styles.button2}>Carrito ({cartItems.length})</button>
+        </Link>
       </div>
     </div>
   );
 };
 
 export default Nav;
-
-
