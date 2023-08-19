ea800003ffc73f612ed0955c6ee598d12bb50ba9
CarlaMelina <cmps03.5@gmail.com>
2023-08-19 12:12:06 -0300

    hashing password  model user

--- api/src/dbMongodb.js
+++ api/src/dbMongodb.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 var mongoose = require('mongoose');
+const { MONGO_URI } = require ("../env")
+
 async function connectToDatabase() {
   await mongoose
-    .connect('mongodb+srv://MarketZone:ZoneMarket@cluster0.fyv03ie.mongodb.net/mz_store')
+    .connect(MONGO_URI)
     .then(() => console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online'))
     .catch((e) => console.log('Error de conexion ', e));
 }
