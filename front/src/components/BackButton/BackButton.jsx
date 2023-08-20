7797ab79dae49ebfd0c04f221f6c84833e7c20a2
Martín0211 <martin021191@gmail.com>
2023-08-20 16:51:56 -0600

    se agrega componente de boton ir atras, no funciona aun

---
+++ front/src/components/BackButton/BackButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import MyButton from "../Buttons/MainButton";
+
+const BackButton = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // Redirige a la página anterior
+  };
+
+  return (
+    <Link to="#" onClick={handleGoBack} style={{ textDecoration: "none" }}>
+      <MyButton text="Ir Atras" variant="inicio" type="button" />
+    </Link>
+  );
+};
+
+export default BackButton;
