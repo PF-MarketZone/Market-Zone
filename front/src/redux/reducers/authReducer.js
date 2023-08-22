439694034d9c2bd6ac5b0cfa2bfb372988186780
Martín0211 <martin021191@gmail.com>
2023-08-22 17:35:47 -0600

    se agregan actions y reducer del login

---
+++ front/src/redux/reducers/authReducer.js
@@ -0,0 +1,32 @@
+import {
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    LOGOUT,
+} from "../Actions/authAction"
+
+const initialState = {
+    user: null,
+    isAuthenticated: false,
+  };
+  
+  const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case LOGIN_SUCCESS:
+        return {
+          ...state,
+          user: action.payload,
+          isAuthenticated: true,
+        };
+      case LOGIN_FAILURE:
+      case LOGOUT:
+        return {
+          ...state,
+          user: null,
+          isAuthenticated: false,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default authReducer;
