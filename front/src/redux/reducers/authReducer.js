496fe49b90cca9d4859a016c36a2cd3f47f1b071
Martín0211 <martin021191@gmail.com>
2023-08-23 02:35:51 -0600

    se coloca que el estado inicia dependa de session storage

--- front/src/redux/reducers/authReducer.js
+++ front/src/redux/reducers/authReducer.js
@@ -4,29 +4,37 @@ import {
     LOGOUT,
 } from "../Actions/authAction"
 
+const isAuthenticatedStored = sessionStorage.getItem('isAuthenticated') === 'true';
+
 const initialState = {
-    user: null,
-    isAuthenticated: false,
-  };
-  
-  const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case LOGIN_SUCCESS:
-        return {
-          ...state,
-          user: action.payload,
-          isAuthenticated: true,
-        };
-      case LOGIN_FAILURE:
-      case LOGOUT:
-        return {
-          ...state,
-          user: null,
-          isAuthenticated: false,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default authReducer;
+  user: null,
+  isAuthenticated: isAuthenticatedStored,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        user: action.payload,
+        isAuthenticated: true,
+      };
+    case LOGIN_FAILURE:
+    case LOGOUT:
+      return {
+        ...state,
+        user: null,
+        isAuthenticated: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
+
+
+
+
+
+
