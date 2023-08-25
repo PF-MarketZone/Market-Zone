d02fa22b777f90148488dd23ce866fbe02aeda24
CarlaMelina <cmps03.5@gmail.com>
2023-08-25 15:50:03 -0300

    product model change

--- api/src/models/product.js
+++ api/src/models/product.js
@@ -2,25 +2,16 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema(
   {
-    storeId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    storeId: { type: mongoose.Schema.Types.ObjectId },
     name: { type: String, required: true },
     price: { type: Number, required: true },
     image: [{ url: { type: String, required: true } }],
     description: { type: String, maxLength: 2500 },
     stock: Number,
-    categories: [
-      {
-        category: {
-          type: String,
-          required: true,
-        },
-        subcategories: [
-          {
-            type: String,
-          },
-        ],
-      },
-    ],
+    categories: {type: {
+      category: {type: String, required: true}, 
+      subcategory: {type: String, required: true}
+      }, required: true},
     color: String,
   },
   { collection: 'products' }
