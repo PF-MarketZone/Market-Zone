918a855893f79882877ff0b3b3e291ae05d98611
Martín0211 <martin021191@gmail.com>
2023-08-27 14:10:28 -0600

    se elimina GET PRODUCT BY ID del archivo actions para hacer un independiente ProducActions

--- front/src/redux/actions.js
+++ front/src/redux/actions.js
@@ -4,7 +4,6 @@ export const SET_CATEGORIA_FILTRO = "SET_CATEGORIA_FILTRO";
 export const SET_PRECIO_MIN_FILTRO = "SET_PRECIO_MIN_FILTRO";
 export const SET_PRECIO_MAX_FILTRO = "SET_PRECIO_MAX_FILTRO";
 export const SET_ORDEN_ALFABETICO = "SET_ORDEN_ALFABETICO";
-export const GET_PRODUCT_BY_ID = "GET_PRODUCT_BY_ID";
 export const AGREGAR_AL_CARRITO = "AGREGAR_AL_CARRITO";
 export const ELIMINAR_DEL_CARRITO = "ELIMINAR_DEL_CARRITO";
 export const SET_INITIAL_CART = "SET_INITIAL_CART";
@@ -65,33 +64,3 @@ export const disminuirCantidad = (itemId) => {
     payload: itemId,
   };
 };
-
-
-export const getProductById = (id) => {
-  return function (dispatch, getState) {
-    const state = getState(); // Obtener el estado actual
-    const productDetails = state.filters.details?.find(detail => detail.id === parseInt(id));
-
-    if (productDetails) {
-      dispatch({ type: GET_PRODUCT_BY_ID, payload: productDetails });
-    } else {
-      // Manejar el caso en que el producto no se encuentra en los detalles estáticos
-      console.error(`Producto con ID ${id} no encontrado.`);
-    }
-  };
-};
-
-// descomentar esto cuando se tenga la api ⇓ y comentar esto ⇑
-/* export const getProductById = (id) => {
-  return async function (dispatch) {
-    try {
-      const response = await axios.get(`URL_DE_TU_API/product/${id}`);
-      const productDetails = response.data;
-      dispatch({ type: GET_PRODUCT_BY_ID, payload: productDetails });
-    } catch (error) {
-      // Manejar el error en caso de que la llamada a la API falle
-      console.error("Error al obtener detalles del producto:", error);
-    }
-  };
-};
- */
