da54eafd65aaf217087dcc167bf561f9ce2e1f14
Martín0211 <martin021191@gmail.com>
2023-08-27 14:13:11 -0600

    se crea el componente de footer y sus estilos aparte

---
+++ front/src/components/footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { styled } from "styled-components";
+import Logo from '../../images/Logo.png'
+import { Link } from "react-router-dom";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { DivMain, LogoImg, DivOneLine, DivSocialMedia, DivLineLeft, DivLineCenter, DivLineRigth, H6, DivLastLine } from "./footerStyledComponent" 
+
+
+const Footer = () => {
+    return (
+        <>
+            <DivMain>
+                <DivOneLine>
+                    <DivLineLeft></DivLineLeft>
+                    <DivSocialMedia>
+                        <Link to="https://github.com/PF-MarketZone/Market-Zone.git" style={{textDecoration: "none", color:"#fbfbf3ff"}}>
+                            <AiFillGithub />
+                        </Link>
+                        <Link to="" style={{textDecoration: "none", color:"#fbfbf3ff"}}>
+                            <AiFillLinkedin />
+                        </Link>
+                    </DivSocialMedia>
+                    <DivLineRigth></DivLineRigth>
+                </DivOneLine>
+                <Link to="/home" style={{textDecoration: "none", }}>
+                    <LogoImg
+                        src={Logo}
+                        alt="logo no disponible"
+                    />
+                </Link>
+                <H6>Copiright©2023 MarketZone, inc.</H6>
+                <DivLastLine>
+                    <H6>Informacion legal</H6>
+                    <DivLineCenter/>
+                    <H6>Politicas de privacidad</H6>
+                </DivLastLine>
+            </DivMain>
+        </>
+    )
+
+}
+
+export default Footer;
---
+++ front/src/components/footer/footerStyledComponent.jsx
@@ -0,0 +1,58 @@
+const DivMain = styled.div`
+    align-items: center;
+    width:100% ;
+    height: 28%;
+    padding: 1vw 0;
+    background-color: #1d1e18ff;
+`
+
+const LogoImg = styled.img`
+    width: 12vw;
+`
+
+const DivOneLine = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const DivSocialMedia = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1vw;
+    font-size: 2.5vw;
+    color: #fbfbf3ff;   
+`
+
+const DivLineLeft = styled.div`
+    width: 50%;
+    height: .1vh;
+    margin: 2vw 2vw 0 2vw;
+    background-color: #fbfbf3ff;  
+`
+
+const DivLineCenter= styled.div`
+    display: flex;
+    align-items: center;
+    width: .1vh;
+    height: 2vh;
+    margin: 0 1vh;
+    background-color: #fbfbf3ff;  
+`
+
+const DivLineRigth = styled.div`
+    width: 50%;
+    height: .1vh;
+    margin: 2vw 2vw 0 2vw;
+    background-color: #fbfbf3ff;  
+`
+
+const H6 = styled.h6`
+    margin: .1vw;
+`
+
+const DivLastLine = styled.div`
+    display: flex;
+    margin: 0;
+    justify-content: center;
+    align-items: center;    
+`
