5a6d84f4c124b52de301098e21cf296b4e7ed194
diegofmoreno4 <dfmm17ykty@gmail.com>
2023-08-29 18:29:13 -0500

    bugs fix

--- api/src/models/purchase.js
+++ api/src/models/purchase.js
@@ -1,15 +1,4 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-
-const purchaseSchema = new mongoose.Schema({
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true,},
-  products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Products',},],
-  totalPrice: {type: Number,required: true,},
-  paymentMethod: {type: String, required: true,},
-  transactionDate: Date,
-  transactionStatus: String,
-}, {collection: 'purchase',});
-=======
 const purchaseSchema = new mongoose.Schema(
   {
     user: {
@@ -25,7 +14,6 @@ const purchaseSchema = new mongoose.Schema(
   },
   { collection: 'purchase' }
 );
->>>>>>> d8933b61361f9f7090de45b2d22614303493f3a5
 
 const Purchase = mongoose.model('Purchase', purchaseSchema);
 
