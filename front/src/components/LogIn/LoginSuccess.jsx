2216f3eff813e5d3d2839bd890f91446ce3043b1
Martín0211 <martin021191@gmail.com>
2023-08-29 17:46:03 -0600

    se agrega componente LogInSuccess

---
+++ front/src/components/LogIn/LoginSuccess.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+
+const LogInSuccess = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(location.search);
+        const user = searchParams.get("session");
+        console.log(user);
+
+        if (user) {
+            sessionStorage.setItem("session-mz", user);
+            navigate("/home");
+        }
+    }, []);
+
+
+    return (
+        <div>
+            <p>Procesando...</p>
+        </div>
+    );
+}
+
+export default LogInSuccess;
