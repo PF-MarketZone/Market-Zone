1d8c80497d4dc893c69f4ad9bb2b5246b01900fa
CarlaMelina <cmps03.5@gmail.com>
2023-09-01 13:07:47 -0300

    se agrega nuevo modelo para coleccion Sale- Funcion crearSale para aplicar en venta exitosa

--- api/src/controllers/orderController.js
+++ api/src/controllers/orderController.js
@@ -1,6 +1,19 @@
 const Order = require('../models/order');
 const User = require('../models/Users');
+const Sale =require('../models/sales')
+const Product = require('./models/Product');
 const { sendMail } = require('./emailController');
+// Supongamos que tienes el userId para una venta
+const userId = 'userID';
+
+// Carga el usuario correspondiente desde la base de datos
+const user = await User.findOne({ _id: userId });
+
+// Ahora, puedes acceder al campo city del usuario
+const userCity = user.city;
+
+// userCity contiene la ciudad del usuario que realizó la compra
+
 
 
 const createOrder = async (orderInfo) => {
@@ -8,7 +21,19 @@ const createOrder = async (orderInfo) => {
   };
   
   const createSale = async (saleInfo) => {
-   
+  const products = await Product.find({ _id: { $in: saleInfo.productIds } }); 
+  const storeIds = products.map((product) => product.storeId);
+  const user = await User.findOne({ _id: saleInfo.userId });
+  const userCity = user.address.city;
+    const sale = new Sale({
+      userId: saleInfo.userId, //saco de resp mp
+      date: saleInfo.date, //saco de resp mp
+      amount: saleInfo.amount, //saco de resp mp
+      productIds: saleInfo.productIds,//saco de resp mp
+      storeIds: storeIds,//lo saco de los products,
+      city: userCity,
+    });
+    await sale.save();
   };
 
 
