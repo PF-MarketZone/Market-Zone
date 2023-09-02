08b1f0a73d696583d11749c655801e0e9c4cd36c
CarlaMelina <cmps03.5@gmail.com>
2023-09-02 20:15:02 -0300

    Se crean funciones de envio de mail en caso de compra extosa y compra rechazada

--- api/src/controllers/orderResponseController.js
+++ api/src/controllers/orderResponseController.js
@@ -6,7 +6,9 @@ const Store =require('../models/sales')
 const { sendMail } = require('./emailController');
 
 const {emailOkOrderComprador,
-  emailOkOrderVendedor} = require('../helpers/emailData')
+  emailOkOrderVendedor,
+  emailRejectedOrderComprador
+} = require('../helpers/emailData')
 
 
 
@@ -28,7 +30,7 @@ const createOrder = async (orderInfo) => {
   const createSale = async (saleInfo) => {
   const products = await Products.find({ _id: { $in: saleInfo.productIds } }); 
   const storeIds = products.map((product) => product.storeId);
-  //----xx----
+  //----xx----//
   const user = await User.findOne({ _id: saleInfo.userId });
   const userCity = user.address.city;
     const sale = new Sale({
@@ -37,68 +39,89 @@ const createOrder = async (orderInfo) => {
       amount: saleInfo.amount, //saco de resp mp
       productIds: saleInfo.productIds,//saco de resp mp
       storeIds: storeIds,//lo saco de los products,
-      city: userCity,
+      city: userCity,//saco de products-->user-->city
     });
     await sale.save();
   };
 
   //EMAIL al comprador --->Ok venta
-  const sendConfirmationEmailBuyer= async (order)=>{
+  const sendConfirmationEmailBuyer= async ( order)=>{
     const userId = order.user; //id del comprador
     const user = await User.findById(userId);//traigo la info del comprador para luego hacer uso de la misma ene l envio dle email
     if (!user) {
       throw new Error('Usuario no encontrado');
     };
-    // Obtén las ID de los productos de la orden
+    //  id de los productos de la orden
     const productIds = order.products;
-
-    // Busca los productos en la base de datos utilizando las ID
+    // Busca los productos en la bdd x id
     const products = await Products.find({ _id: { $in: productIds } });
+
 
     const infoMail = await sendMail(emailOkOrderComprador(user, products, order));//envio el email pasandole el componente del cuerpo dle email, al que le paso la info de la order y del user.
     return infoMail;
     
   };
    //EMAIL al vendedor((hay que llegar de idProduct a user.email!!!)) --->Ok venta
+
+   const userSeller= async (productsIds)=>{
+   
+    // Busco los productos en la bdd por id
+        const products = await Products.find({ _id: { $in: productsIds } });
+    // saco losid de las tiendas de los productos
+        const storeIds = products.map((product) => product.storeId);
+         // Busco las tiendas en la bdd x id
+        const stores = await Store.find({ _id: { $in: storeIds } });
+        // Saco los id de los usuarios vendedores de las store
+        const sellerUserIds = stores.map((store) => store.user);
+        // Busco los usuarios vendedores en la bdd usando los id
+        const sellerUsers = await User.find({ _id: { $in: sellerUserIds } });
+        return sellerUsers;
+
+   };
+   const storeName = async(productsIds)=>{
+    // Busco los productos en la bdd por id
+    const products = await Products.find({ _id: { $in: productsIds } });
+    // saco losid de las tiendas de los productos
+        const storeIds = products.map((product) => product.storeId);
+         // Busco las tiendas en la bdd x id
+        const stores = await Store.find({ _id: { $in: storeIds } });
+        const storeNames = stores.map((store) => store.name);
+    return storeNames;
+   }
    const sendConfirmationEmailSeller= async (order)=>{
     
-      try {
-        
-        const productIds = order.products;
-    
-        // Busca los productos en la bdd por id
-        const products = await Products.find({ _id: { $in: productIds } });
-    
-        // Extrae las ID de las tiendas de los productos
-        const storeIds = products.map((product) => product.storeId);
-    
-        // Busca las tiendas en la base de datos utilizando las ID
-        const stores = await Store.find({ _id: { $in: storeIds } });
-    
-        // Extrae las ID de los usuarios vendedores de las tiendas
-        const sellerUserIds = stores.map((store) => store.user);
-    
-        // Busca los usuarios vendedores en la base de datos utilizando las ID
-        const sellerUsers = await User.find({ _id: { $in: sellerUserIds } });
-    
-    
-        const infoMail = await sendMail(emailOkOrderVendedor(sellerUsers, order));
-    
-          
-          
-    
-          return infoMail;
-       
-      } catch (error) {
-        // Manejo de errores
-      }
+        const productsId = order.products.id;
+        const storeNames= storeName(productsId)
+        const sellers = await userSeller(productsId);
+        const infoMail = await sendMail(emailOkOrderVendedor(sellers, order, storeNames));
+        return infoMail;
+         
+     
   };
+   //EMAIL al comprador ---> Venta rechazada
+   const sendRejectedEmailBuyer= async (order)=>{
+    const userId = order.user; 
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new Error('Usuario no encontrado');
+    };
+    const productIds = order.products;
+   
+    const products = await Products.find({ _id: { $in: productIds } });
+
+
+    const infoMail = await sendMail(emailRejectedOrderComprador(user, products, order));
+    return infoMail;
+    
+  };
+  
 
 
 module.exports= {
     createOrder,
     createSale,
     sendConfirmationEmailBuyer,
-    sendConfirmationEmailSeller
+    sendConfirmationEmailSeller,
+    sendRejectedEmailBuyer
 
 }
--- api/src/controllers/paymentController.js
+++ api/src/controllers/paymentController.js
@@ -15,7 +15,7 @@ const createPreference = async (req) => {
       unit_price: item.unit_price,
       quantity: item.quantity,
     })),
-    notification_url: `https://4a9b-190-15-201-57.ngrok.io/api/v1/create-order/noification/`,
+    notification_url: `https://4a9b-190-15-201-57.ngrok.io/api/v1/create-order/notification/`,
     back_urls: {
       success: 'http://localhost:3004/api/v1/create-order/success', // crear componente para compra exitosa
       failure: 'http://localhost:5173/cart',
