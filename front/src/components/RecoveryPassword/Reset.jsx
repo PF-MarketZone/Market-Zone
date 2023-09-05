7e3693ca2d9e5a7cf2e3ac6acca19920e8e29f0e
CarlaMelina <cmps03.5@gmail.com>
2023-09-05 00:08:35 -0300

    Se agrega validacion de email al envío deenlace para resetPassword

--- front/src/components/RecoveryPassword/Reset.jsx
+++ front/src/components/RecoveryPassword/Reset.jsx
@@ -2,16 +2,24 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { StyledForm, Input, P, H3, H5, Div, DivPrincipalReset, CenteredContainer  } from "../LogIn/LogInStyledComponent";
+import { StyledForm, Input, P, H3, H5, Div, DivPrincipal } from "../LogIn/LogInStyledComponent";
 
 const Reset = () => {
   
   const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
+  const [isValidEmail, setIsValidEmail] = useState(false);
 
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+    const newEmail = event.target.value; 
+    setEmail(newEmail);
+    setIsValidEmail(emailRegex.test(newEmail));
   };
+
+  
+  
+
 
   const handleResetRequest = async () => {
     try {
@@ -24,6 +32,7 @@ const Reset = () => {
 
       if (response.status === 200) {
         toast.success('¡Enlace enviado!');
+        setEmail('');
       } else {
         toast.error('Error al enviar el enlace.');
       }
@@ -35,26 +44,25 @@ const Reset = () => {
   };
 
   return (
-    <DivPrincipalReset>
-      <CenteredContainer>
+    <DivPrincipal>
+      
         <ToastContainer />
       <h3>¿Tienes problemas para iniciar sesión?</h3>
       <h5>Ingresa tu correo electrónico y te enviaremos un enlace para que recuperes el acceso a tu cuenta.</h5>
-<StyledForm> <Input
+<StyledForm> <input
         type="email"
         placeholder="Correo electrónico"
         value={email}
         onChange={handleEmailChange}
-      />
-      </StyledForm>
+      /></StyledForm>
      
 
-      <button onClick={handleResetRequest} disabled={loading}>
+     <button onClick={handleResetRequest} disabled={!isValidEmail || loading}>
         {loading ? 'Enviando...' : 'Enviar enlace'}
       </button>
-      </CenteredContainer>
       
-    </DivPrincipalReset>
+      
+    </DivPrincipal>
   );
 };
 
