707ce7ee7dcfd110f9f8cdfe0727855d0f33e465
Martín0211 <martin021191@gmail.com>
2023-09-06 19:05:15 -0600

    edicion de la action de put a post

--- front/src/redux/Actions/userAction.js
+++ front/src/redux/Actions/userAction.js
@@ -5,9 +5,10 @@ import { backendUrl } from '../../deployConfig';
 export const PUT_INFO_PROFILE = 'PUT_INFO_PROFILE';
 
 export const putInfoProfile = (formData) => {
+  console.log(formData)
   return async function (dispatch) {
     try {
-      const apiData = await axios.put(`${backendUrl}/user/modify`, formData);
+      const apiData = await axios.post(`${backendUrl}/user/modify`, formData);
       const modifiProfile = apiData.data;
       dispatch({ type: PUT_INFO_PROFILE, payload: modifiProfile });
     } catch (error) {
